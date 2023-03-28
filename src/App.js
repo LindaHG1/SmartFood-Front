4eb097a981f36ffd7622fd268557f24b898a2297
May Sanchez <114532543+MaySanCal@users.noreply.github.com>
2023-03-28 12:12:37 +0200

    formulario

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-import Footer from './Components/Footer'
+import ContactForm from './Components/ContactForm';
+import Footer from './Components/Footer';
+
 
 function App() {
   return (
     <div className="App">
+      <ContactForm />
       <Footer />
     </div>
   );

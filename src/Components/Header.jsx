c4397622d3714a2413ccf884f0b632e8d0b2f348
LindaHG1 <116809226+LindaHG1@users.noreply.github.com>
2023-03-31 10:27:09 +0200

    variable de colores

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,35 +1,50 @@
 import React from 'react';
 import '../assets/sass/components/_header.scss';
 import banner from '../assets/images/banner.png'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // importa los estilos de AOS
+import { useEffect } from 'react';
 
 function HomePage() {
+    useEffect(() => {
+        AOS.init({duration:1000}); // inicializa AOS
+    }, []);
+    
     return (
         <div className="homepage">
+            
+            <div className="slogan">
+            <div data-aos="fade-left">
+
+                <h1>"En Smarfood encontrarás la frescura de la huerta a solo unos pasos de tu hogar".</h1>
+                <p>Explora lo que ofrecemos</p>
+            </div>
+            </div>
+
             <div className="banner">
-            <img src={ banner } alt="Imagen de la tarjeta" />
-                {/* <h1>Bienvenido a mi sitio web</h1>
-                <p>Explora lo que tengo para ofrecer</p> */}
+                <img src={banner} alt="Imagen de la tarjeta" />
+
             </div>
             <div className="card-container">
                 <div className="card">
                     <img src="https://via.placeholder.com/150" alt="Imagen de la tarjeta" />
-                    
-                    <p>Descripción de la tarjeta 1</p>
+
+                    <p>Cítricos</p>
                 </div>
                 <div className="card">
                     <img src="https://via.placeholder.com/150" alt="Imagen de la tarjeta" />
-                    
-                    <p>Descripción de la tarjeta 2</p>
+
+                    <p>Frutas de temporada</p>
                 </div>
                 <div className="card">
                     <img src="https://via.placeholder.com/150" alt="Imagen de la tarjeta" />
-                    
-                    <p>Descripción de la tarjeta 3</p>
+
+                    <p>Tropilaces</p>
                 </div>
                 <div className="card">
                     <img src="https://via.placeholder.com/150" alt="Imagen de la tarjeta" />
-                    
-                    <p>Descripción de la tarjeta 4</p>
+
+                    <p>Frutos Secos</p>
                 </div>
             </div>
         </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,8 +5,10 @@ import { menu } from "react-icons-kit/feather/menu";
 import { x } from "react-icons-kit/feather/x";
 import logo from '../assets/images/logo.png'
 
+
 export const Navbar = () => {
     const [toggle, setToggle] = useState(false);
+
 
     const handleToggle = () => {
         setToggle(!toggle);
@@ -19,9 +21,15 @@ export const Navbar = () => {
                 {toggle ? <Icon icon={x} size={28} /> : <Icon icon={menu} size={28} />}
             </div>
             <ul className="links">
-                <li className="float-shadow">Inicio</li>
-                <li className="float-shadow">Productos</li>
-                <li className="float-shadow">Contacto</li>
+                <li className="float-shadow">
+                    <a href="/">Inicio</a>
+                </li>
+                <li className="float-shadow">
+                    <a href="/productos">Productos</a>
+                </li>
+                <li className="float-shadow">
+                    <a href="/contacto">Contacto</a>
+                </li>
             </ul>
         </nav>
     );

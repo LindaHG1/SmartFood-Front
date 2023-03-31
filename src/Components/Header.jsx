64cd50b2f2b3bbf4c759f17b6980f8f5bb56bf0a
LindaHG1 <116809226+LindaHG1@users.noreply.github.com>
2023-03-31 15:30:03 +0200

    banner modificado

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,6 +4,7 @@ import banner from '../assets/images/banner.png'
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // importa los estilos de AOS
 import { useEffect } from 'react';
+import banner2 from '../assets/images/banner2.png'
 
 function HomePage() {
     useEffect(() => {
@@ -13,39 +14,44 @@ function HomePage() {
     return (
         <div className="homepage">
             <div className="banner">
-                <img src={banner} alt="Imagen de la tarjeta" />
-                <div className="slogan">
-                    <div data-aos="fade-left">
-                        <h1>"En Smarfood encontrarás la frescura de la huerta a solo unos pasos de tu hogar".</h1>
-                        <p>Explora lo que ofrecemos</p>
+                <picture>
+                    <source media="(max-width: 745px)" srcset= {banner2}/>
+                    <img src={banner} alt="Imagen de la tarjeta" />
+                        </picture>
+
+                        
+                        <div className="slogan">
+                            <div data-aos="fade-left">
+                                <h1>"En Smarfood encontrarás la frescura de la huerta a solo unos pasos de tu hogar".</h1>
+                                <p>Explora lo que ofrecemos</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                    <div className="card-container">
+                        <div className="card">
+                            <img src="https://via.placeholder.com/150" alt="Imagen de la tarjeta" />
+
+                            <p>Cítricos</p>
+                        </div>
+                        <div className="card">
+                            <img src="https://via.placeholder.com/150" alt="Imagen de la tarjeta" />
+
+                            <p>Frutas de temporada</p>
+                        </div>
+                        <div className="card">
+                            <img src="https://via.placeholder.com/150" alt="Imagen de la tarjeta" />
+
+                            <p>Tropicales</p>
+                        </div>
+                        <div className="card">
+                            <img src="https://via.placeholder.com/150" alt="Imagen de la tarjeta" />
+
+                            <p>Frutos Secos</p>
+                        </div>
+
+                    </div>
             </div>
-            <div className="card-container">
-                <div className="card">
-                    <img src="https://via.placeholder.com/150" alt="Imagen de la tarjeta" />
-
-                    <p>Cítricos</p>
-                </div>
-                <div className="card">
-                    <img src="https://via.placeholder.com/150" alt="Imagen de la tarjeta" />
-
-                    <p>Frutas de temporada</p>
-                </div>
-                <div className="card">
-                    <img src="https://via.placeholder.com/150" alt="Imagen de la tarjeta" />
-
-                    <p>Tropicales</p>
-                </div>
-                <div className="card">
-                    <img src="https://via.placeholder.com/150" alt="Imagen de la tarjeta" />
-                    
-                    <p>Frutos Secos</p>
-                </div>
-
-            </div>
-        </div>
-    );
+            );
 }
 
-export default HomePage;
+            export default HomePage;
